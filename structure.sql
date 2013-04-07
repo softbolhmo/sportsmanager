bbff54fba03d7e6cb105a56b9925e3dea3a9b6a6
lacroixca <lacroixca@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-07 22:18:25 +0000

    Updated structure.
    git-svn-id: https://plugins.svn.wordpress.org/sportsmanager/trunk@693248 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- structure.sql
+++ structure.sql
@@ -1,4 +1,5 @@
-CREATE TABLE wp_sportsmanager_clubs (
+DROP TABLE IF EXISTS wp_sportsmanager_clubs;
+CREATE TABLE IF NOT EXISTS wp_sportsmanager_clubs (
   id int(11) NOT NULL AUTO_INCREMENT,
   league_id int(11) NOT NULL,
   sport varchar(100) NOT NULL,
@@ -10,7 +11,8 @@ CREATE TABLE wp_sportsmanager_clubs (
   PRIMARY KEY (id)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
-CREATE TABLE wp_sportsmanager_games (
+DROP TABLE IF EXISTS wp_sportsmanager_games;
+CREATE TABLE IF NOT EXISTS wp_sportsmanager_games (
   id int(11) NOT NULL AUTO_INCREMENT,
   league_id int(11) NOT NULL,
   season year(4) NOT NULL,
@@ -27,7 +29,8 @@ CREATE TABLE wp_sportsmanager_games (
   PRIMARY KEY (id)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
-CREATE TABLE wp_sportsmanager_leagues (
+DROP TABLE IF EXISTS wp_sportsmanager_leagues;
+CREATE TABLE IF NOT EXISTS wp_sportsmanager_leagues (
   id int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   slug varchar(100) NOT NULL,
@@ -37,7 +40,8 @@ CREATE TABLE wp_sportsmanager_leagues (
   PRIMARY KEY (id)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
-CREATE TABLE wp_sportsmanager_locations (
+DROP TABLE IF EXISTS wp_sportsmanager_locations;
+CREATE TABLE IF NOT EXISTS wp_sportsmanager_locations (
   id int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   slug varchar(100) NOT NULL,
@@ -47,13 +51,15 @@ CREATE TABLE wp_sportsmanager_locations (
   PRIMARY KEY (id)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
-CREATE TABLE wp_sportsmanager_players (
+DROP TABLE IF EXISTS wp_sportsmanager_players;
+CREATE TABLE IF NOT EXISTS wp_sportsmanager_players (
   id int(11) NOT NULL AUTO_INCREMENT,
   user_id int(11) NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
-CREATE TABLE wp_sportsmanager_scoresheets (
+DROP TABLE IF EXISTS wp_sportsmanager_scoresheets;
+CREATE TABLE IF NOT EXISTS wp_sportsmanager_scoresheets (
   id int(11) NOT NULL AUTO_INCREMENT,
   league_id int(11) NOT NULL,
   season year(4) NOT NULL,
@@ -64,7 +70,8 @@ CREATE TABLE wp_sportsmanager_scoresheets (
   PRIMARY KEY (id)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
-CREATE TABLE wp_sportsmanager_teams (
+DROP TABLE IF EXISTS wp_sportsmanager_teams;
+CREATE TABLE IF NOT EXISTS wp_sportsmanager_teams (
   id int(11) NOT NULL AUTO_INCREMENT,
   club_id int(11) NOT NULL,
   season year(4) NOT NULL,
