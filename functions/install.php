dde8f54c4e5a0bc66e4909d7bce6d797452bae45
lacroixca <lacroixca@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-08 02:24:56 +0000

    Updated installation.
    git-svn-id: https://plugins.svn.wordpress.org/sportsmanager/trunk@693401 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- functions/install.php
+++ functions/install.php
@@ -2,20 +2,81 @@
 
 function sm_install_plugin() {
 	global $wpdb;
-	$table = $wpdb->prefix.SPORTSMANAGER_PREFIX."clubs"; 
-	$q = "CREATE TABLE $table (
-		id int(11) NOT NULL AUTO_INCREMENT,
-		league_id int(11) NOT NULL,
-		sport varchar(100) NOT NULL,
-		name varchar(100) NOT NULL,
-		slug varchar(100) NOT NULL,
-		description text NOT NULL,
-		small_logo_url varchar(256) NOT NULL,
-		large_logo_url varchar(256) NOT NULL,
-		PRIMARY KEY  (id)
-	);";
 	require_once(ABSPATH.'wp-admin/includes/upgrade.php');
-	dbDelta($q);
+	$queries = array (
+		'clubs'			=> "CREATE TABLE @table_name@ (
+								id int(11) NOT NULL AUTO_INCREMENT,
+								league_id int(11) NOT NULL,
+								sport varchar(100) NOT NULL,
+								name varchar(100) NOT NULL,
+								slug varchar(100) NOT NULL,
+								description text NOT NULL,
+								small_logo_url varchar(256) NOT NULL,
+								large_logo_url varchar(256) NOT NULL,
+								PRIMARY KEY  (id)
+							);",
+		'games'			=> "CREATE TABLE @table_name@ (
+								id int(11) NOT NULL AUTO_INCREMENT,
+								league_id int(11) NOT NULL,
+								season year(4) NOT NULL,
+								sport varchar(100) NOT NULL,
+								home_team_id int(11) NOT NULL,
+								away_team_id int(11) NOT NULL,
+								home_score int(11) NOT NULL,
+								away_score int(11) NOT NULL,
+								winner_team_id int(11) NOT NULL,
+								date datetime NOT NULL,
+								type varchar(2) NOT NULL,
+								location_id int(11) NOT NULL,
+								cancelled int(1) NOT NULL,
+								PRIMARY KEY  (id)
+							)",
+		'leagues'		=> "CREATE TABLE @table_name@ (
+								id int(11) NOT NULL AUTO_INCREMENT,
+								name varchar(100) NOT NULL,
+								slug varchar(100) NOT NULL,
+								description text NOT NULL,
+								small_logo_url varchar(256) NOT NULL,
+								large_logo_url varchar(256) NOT NULL,
+								PRIMARY KEY  (id)
+							)",
+		'locations'		=> "CREATE TABLE @table_name@ (
+								id int(11) NOT NULL AUTO_INCREMENT,
+								name varchar(100) NOT NULL,
+								slug varchar(100) NOT NULL,
+								description text NOT NULL,
+								small_logo_url varchar(256) NOT NULL,
+								large_logo_url varchar(256) NOT NULL,
+								PRIMARY KEY  (id)
+							)",
+		'players'		=> "CREATE TABLE @table_name@ (
+								id int(11) NOT NULL AUTO_INCREMENT,
+								user_id int(11) NOT NULL,
+								PRIMARY KEY  (id)
+							)",
+		'scoresheets'	=> "CREATE TABLE @table_name@ (
+								id int(11) NOT NULL AUTO_INCREMENT,
+								league_id int(11) NOT NULL,
+								season year(4) NOT NULL,
+								sport varchar(100) NOT NULL,
+								game_id int(11) NOT NULL,
+								player_id int(11) NOT NULL,
+								stats text NOT NULL,
+								PRIMARY KEY  (id)
+							)",
+		'teams'			=> "CREATE TABLE @table_name@ (
+								id int(11) NOT NULL AUTO_INCREMENT,
+								club_id int(11) NOT NULL,
+								season year(4) NOT NULL,
+								players_id text NOT NULL,
+								PRIMARY KEY  (id)
+							)"
+	);
+	foreach (array ('clubs', 'games', 'leagues', 'locations', 'players', 'scoresheets', 'teams') as $table) {
+		$table_name = $wpdb->prefix.SPORTSMANAGER_PREFIX.$table;
+		$q = str_replace('@table_name@', $table_name, $queries[$table]);
+		dbDelta($q);
+	};
 }
 
 register_activation_hook(SPORTSMANAGER_DIR.'sportsmanager.php', 'sm_install_plugin');
