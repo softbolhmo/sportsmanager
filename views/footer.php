a62f38471eb0be6b107ff3ec3dd6a88e0ec0937f
lacroixca <lacroixca@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-08 17:47:35 +0000

    Updated footer.
    git-svn-id: https://plugins.svn.wordpress.org/sportsmanager/trunk@693811 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/footer.php
+++ views/footer.php
@@ -1,7 +1,11 @@
 
 </div><!--end .sm_page_inside-->
 <div class="sm_page_footer">
-	<p>Sports Manager ©</p>
+	<div>
+		<span class="left">Sports Manager ©</span>
+		<span class="right"><a href="http://wordpress.org/extend/plugins/sportsmanager/changelog/" target="_blank">Change Log</a> | <a href="http://wordpress.org/support/plugin/sportsmanager" target="_blank">Support</a></span>
+	</div>
+	<div class="clear"></div>
 </div><!--end .sm_page_footer-->
 
 	</div><!--end .sm_inner_container-->
