3b6de92dfe10a92bc836fed3c9005d2a1a5772b0
lacroixca <lacroixca@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-15 19:40:50 +0000

    Correction.
    git-svn-id: https://plugins.svn.wordpress.org/sportsmanager/trunk@698000 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- controllers/header_scripts.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$sports = array('baseball', 'basketball', 'football', 'soccer', 'tennis');
-$rand = rand(0, 4);
-$favicon = SPORTSMANAGER_URL.'images/sports/'.$sports[$rand].'.ico';
-$css = SPORTSMANAGER_URL.'css/style.css';
-$js = SPORTSMANAGER_URL.'js/scripts.php';
--- controllers/header_styles.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$sports = array('baseball', 'basketball', 'football', 'soccer', 'tennis');
-$rand = rand(0, 4);
-$menu_icon = SPORTSMANAGER_URL.'images/sports/'.$sports[$rand].'.png';
